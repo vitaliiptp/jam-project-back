353472c3b260955c5d833c07a355e9e059d331b6
Eike Idel <88095420+eikeIdel@users.noreply.github.com>
2021-11-25 17:33:40 +0100

    new sql dump file

--- jam_jam.sql
+++ jam_jam.sql
@@ -19,36 +19,6 @@ CREATE TABLE `follow` (
   CONSTRAINT `fk_user_follow_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `genre` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `genre` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `genre_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `instrument` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `instrument` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `instrument_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-CREATE TABLE `music_samples` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `music_sample` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `user_id` (`user_id`),
-  CONSTRAINT `music_samples_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `profiles` (
   `id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -60,6 +30,9 @@ CREATE TABLE `profiles` (
   `contact_phone` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `contact_mail` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `biography` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `instrument` varchar(255) DEFAULT NULL,
+  `genre` varchar(255) DEFAULT NULL,
+  `music_sample` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `user_id` (`user_id`),
@@ -85,49 +58,15 @@ INSERT INTO `follow` (`id`, `user_id`, `follow_user_id`) VALUES
 (4, 2, 1),
 (5, 3, 1);
 
-INSERT INTO `genre` (`id`, `user_id`, `genre`) VALUES
-(1, 1, 'jazz');
-INSERT INTO `genre` (`id`, `user_id`, `genre`) VALUES
-(2, 1, 'blues');
-INSERT INTO `genre` (`id`, `user_id`, `genre`) VALUES
-(3, 2, 'jazz');
-INSERT INTO `genre` (`id`, `user_id`, `genre`) VALUES
-(4, 3, 'blues'),
-(5, 4, 'jazz'),
-(6, 5, 'rock'),
-(7, 3, 'rock'),
-(8, 1, 'rock');
-
-INSERT INTO `instrument` (`id`, `user_id`, `instrument`) VALUES
-(1, 1, 'singer');
-INSERT INTO `instrument` (`id`, `user_id`, `instrument`) VALUES
-(2, 1, 'guitar');
-INSERT INTO `instrument` (`id`, `user_id`, `instrument`) VALUES
-(3, 2, 'bass');
-INSERT INTO `instrument` (`id`, `user_id`, `instrument`) VALUES
-(4, 2, 'drums'),
-(5, 3, 'guitar'),
-(6, 3, 'bass'),
-(7, 4, 'violin'),
-(8, 5, 'saxophon'),
-(9, 5, 'flute');
-
-INSERT INTO `music_samples` (`id`, `user_id`, `music_sample`) VALUES
-(1, 1, 'https://soundcloud.com/introspectivachannel/o-floating-machine-o');
-INSERT INTO `music_samples` (`id`, `user_id`, `music_sample`) VALUES
-(2, 1, 'https://soundcloud.com/introspectivachannel/saebius-a-thousand-changes');
-INSERT INTO `music_samples` (`id`, `user_id`, `music_sample`) VALUES
-(3, 2, 'https://soundcloud.com/jaycoba/space-disco-session-2-vinyl-by-simplexia');
-
-INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`) VALUES
-(1, 1, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'beginner', '1233', 'user1@gmail.com', 'blablablab');
-INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`) VALUES
-(2, 2, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'beginner', '3454534', 'user2@gmail.com', 'blablablab');
-INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`) VALUES
-(3, 3, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'beginner', '234234', 'user3@gmail.com', 'blablablab');
-INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`) VALUES
-(4, 4, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'advance', '153452', 'user4@gmail.com', 'blablablab'),
-(5, 5, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'advance', '2354234', 'user5@gmail.com', 'blablablab');
+INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`, `instrument`, `genre`, `music_sample`) VALUES
+(1, 1, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'beginner', '1233', 'user1@gmail.com', 'blablablab', 'guitar', NULL, 'https://soundcloud.com/introspectivachannel/saebius-a-thousand-changes');
+INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`, `instrument`, `genre`, `music_sample`) VALUES
+(2, 2, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'beginner', '3454534', 'user2@gmail.com', 'blablablab', 'bass', NULL, 'https://soundcloud.com/introspectivachannel/o-floating-machine-o');
+INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`, `instrument`, `genre`, `music_sample`) VALUES
+(3, 3, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'beginner', '234234', 'user3@gmail.com', 'blablablab', 'drum', NULL, 'https://soundcloud.com/jaycoba/space-disco-session-2-vinyl-by-simplexia');
+INSERT INTO `profiles` (`id`, `user_id`, `picture`, `city`, `latitude`, `longitude`, `skill_level`, `contact_phone`, `contact_mail`, `biography`, `instrument`, `genre`, `music_sample`) VALUES
+(4, 4, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'advance', '153452', 'user4@gmail.com', 'blablablab', 'singer', NULL, NULL),
+(5, 5, 'https://dummyimage.com/200x280', 'Berlin', NULL, NULL, 'advance', '2354234', 'user5@gmail.com', 'blablablab', 'gutar', NULL, NULL);
 
 INSERT INTO `user` (`id`, `user_name`, `password`, `email`) VALUES
 (1, 'user1', '1234', 'user1@gmail.com');
